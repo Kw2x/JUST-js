c68e62d6c48927788a6248196adcaaae767e93e8
kw2x <bhen2014@gmail.com>
2022-09-15 19:24:48 -0400

    third script

---
+++ third.js
@@ -0,0 +1,12 @@
+let age = 30;
+
+if((!age > 14 && age < 91)) {
+    console.log(`${age} is not in range`)
+}
+else{
+    console.log(`${age} is in range`)
+}
+
+// second variant 
+
+if (age < 14 || age > 90)
