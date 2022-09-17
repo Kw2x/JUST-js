0eea5758f563be84f760c1ef225e414e62c0760a
kw2x <bhen2014@gmail.com>
2022-09-17 19:26:09 -0400

    testing to see if the first 3 months acted as expected

---
+++ fith.js
@@ -0,0 +1,38 @@
+// Calendar App
+
+const select = document.querySelector('select');
+const list = document.querySelector('ul');
+const h1 = document.querySelector('h1');
+
+select.addEventListener('change', () => {
+  const choice = select.value;
+
+  // ADD CONDITIONAL HERE
+  let month = choice
+  let days = 31
+
+  if(month === "Janurary" || month === "March" || month === "May"){
+    days = 31
+  }
+  else{
+    days = 30
+  }
+ 
+
+
+
+  createCalendar(days, choice);
+});
+
+function createCalendar(days, choice) {
+  list.innerHTML = '';
+  h1.textContent = choice;
+  for (let i = 1; i <= days; i++) {
+    const listItem = document.createElement('li');
+    listItem.textContent = i;
+    list.appendChild(listItem);
+  }
+}
+
+createCalendar(31,'January');
+
